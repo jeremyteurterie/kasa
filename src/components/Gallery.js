2a8fce39cca373c5bb9c12979943ae1071ca397a
Jeremy <jeremy.teurterie@gmail.com>
2023-03-15 17:23:45 +0700

    :recycle: refactor: gallery

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,38 +1,21 @@
-import { useState } from 'react';
-import style from '../styles/Gallery.module.css';
+import React, { useState } from 'react';
 import { ReactComponent as LeftArrow } from '../assets/arrowLeft.svg';
 import { ReactComponent as RightArrow } from '../assets/arrowRight.svg';
+import style from '../styles/Gallery.module.css';
 
 const Gallery = ({ imageArray, desc }) => {
   const [currentIndex, setCurrentIndex] = useState(0);
-
-  // if there is only one photo, hide arrow buttons
-  // s'il n'y a qu'une seule photo, cacher les boutons fléchés
   const displayArrow = imageArray.length > 1;
-
-  // if current index is smaller than length, show next image
-  // else show the first image
-  // si l'index actuel est inférieur à la longueur, afficher l'image suivante.
-  // Sinon, afficher la première image
-  const next = () => {
-    if (currentIndex < imageArray.length - 1) {
-      setCurrentIndex((prevState) => prevState + 1);
-    } else {
-      setCurrentIndex(0);
-    }
+  const getNextIndex = (currentIndex, arrayLength) => {
+    return currentIndex < arrayLength - 1 ? currentIndex + 1 : 0;
   };
-
-  // if current index is bigger than 0, current index - 1
-  // else show the last image
-  // si l'index actuel est supérieur à 0, index actuel - 1
-  // sinon afficher la dernière image
-  const previous = () => {
-    if (currentIndex > 0) {
-      setCurrentIndex((prevState) => prevState - 1);
-    } else {
-      setCurrentIndex(imageArray.length - 1);
-    }
+  const getPrevIndex = (currentIndex, arrayLength) => {
+    return currentIndex > 0 ? currentIndex - 1 : arrayLength - 1;
   };
+  const next = () =>
+    setCurrentIndex((prevIndex) => getNextIndex(prevIndex, imageArray.length));
+  const previous = () =>
+    setCurrentIndex((prevIndex) => getPrevIndex(prevIndex, imageArray.length));
 
   return (
     <div className={style.galleryContainer}>
